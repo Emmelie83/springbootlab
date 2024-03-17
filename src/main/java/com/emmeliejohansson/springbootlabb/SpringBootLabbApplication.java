c1859810e3da9f856cbe9cad20e70f4a4ada2656
Emmelie83 <info@emmeliejohansson.com>
2024-03-17 20:59:59 +0100

    Initial commit

---
+++ src/main/java/com/emmeliejohansson/springbootlabb/SpringBootLabbApplication.java
@@ -0,0 +1,13 @@
+package com.emmeliejohansson.springbootlabb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmeliejohansson/springbootlabb/SpringBootLabbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmeliejohansson.springbootlabb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLabbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
